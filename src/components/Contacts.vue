5e7117843414e873610547bbc7fc2d4edc0b8acc
Ekaterina Rykova <katrykova2002@yandex.ru>
2021-11-21 15:26:21 +0300

    add map

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -3,7 +3,7 @@
   <h2>Наш телефон:</h2>+7 937 34 45 54
   <h2>Как к нам добраться?</h2>
   <div>
-    <img src="" alt="map">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3366.927867665609!2d47.66169179795356!3d45.61774525825468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x41abf5a64498e155%3A0x4811114de8c0648a!2sCaspian%20Lotus!5e0!3m2!1sen!2sru!4v1637497294037!5m2!1sen!2sru" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
   </div>
 </div>
 </template>
